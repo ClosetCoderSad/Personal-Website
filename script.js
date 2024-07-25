cdd05aa6cf41edd8e12492f94139569cbcdd2a0a
ClosetCoderSad <abrarroll2@gmail.com>
2024-07-25 19:41:00 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector(".hamburger_menu_links");
+    const icon = document.querySelector(".hamburger_menu_icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
