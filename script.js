6f8b8f6f7324b6637584fc7512226d7d3f25b0a6
Md Abrar Al Zabir <abrarroll2@gmail.com>
2024-07-30 03:45:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function toggleMenu() {
     const menu = document.querySelector(".hamburger_menu_links");
     const icon = document.querySelector(".hamburger_menu_icon");
+    const link = document.querySelector("li");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-  }
+    link.classList.toggle("open");
+}
 
